73b2158680e2fbfc6bf4b25ca5106b2cfdc2fb93
MOUAD BANI <54271682+mbani01@users.noreply.github.com>
2021-11-10 11:50:54 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
-cp clean.sh ~/;
+curl -fsSL https://raw.githubusercontent.com/mbani01/42_Cache_Cleaner/master/clean.sh > ~/clean.sh;
 echo 'sh ~/clean.sh > /dev/null 2>&1 && printf "\x1B[32m\u2714 \x1B[0mCache Cleaned\\n"' >> ~/.zshrc
